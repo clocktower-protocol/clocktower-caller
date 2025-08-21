2d515e5f954baead13359412878cdaf4c1a9f947
vhmarx <vhmarx@haderachllc.com>
2025-08-21 06:34:56 -0500

    fixed desmond bug

--- clocktower-base-caller/remit_script.js
+++ clocktower-base-caller/remit_script.js
@@ -447,7 +447,7 @@ export default {
     const shouldProceed = await preCheck();
     console.log(`shouldProceed: ${shouldProceed}`);
     if (shouldProceed) {
-      //await desmond();
+      await desmond();
       console.log('PreCheck passed, executing desmond');
     } else {
       console.log('PreCheck failed, skipping desmond execution');
